a281df2c91fa0c17696af73e3b97f7231a96e267
Salman <isalman19@hotmail.com>
2019-11-16 18:51:11 +0100

    Updates

---
+++ precache-manifest.8c157a54312b3d5cc6e9f8507ae58ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a06992d46325cd355f5437475ce24c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1938250aedfd5d1f80e6",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "96fa164cc09ba7e15ca1",
+    "url": "/static/css/main.54c7cfb8.chunk.css"
+  },
+  {
+    "revision": "1938250aedfd5d1f80e6",
+    "url": "/static/js/2.04e197e0.chunk.js"
+  },
+  {
+    "revision": "96fa164cc09ba7e15ca1",
+    "url": "/static/js/main.ec0b4e5c.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
